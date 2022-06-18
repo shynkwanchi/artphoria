341db47d9b3943c70b928b0cbc1d9feea6b5cf16
Nguyen Quang Duy <duynguyenquang412@gmail.com>
2022-06-18 20:59:40 +0700

    Update header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -11,7 +11,15 @@ const Header = () => {
                     <div className="logo"><a href="#">ARTPHORIA</a></div>
                     <ul className="links">
                         <li><a href="#"><em className="fas fa-home nav-icon"></em>Home</a></li>
-                        <li><a href="#"><em className="fas fa-compass nav-icon"></em>Explore</a></li>
+                        <li>
+                            <a href="#" class="desktop-link"><em class="fas fa-compass nav-icon"></em>Explore</a>
+                            <input type="checkbox" id="show-explore" />
+                            <label for="show-explore"><em class="fas fa-compass nav-icon"></em>Explore</label>
+                            <ul>
+                                <li><a href="#"><em class="fas fa-image nav-icon"></em>by Genre</a></li>
+                                <li><a href="#"><em class="fas fa-users nav-icon"></em>by Following</a></li>
+                            </ul>
+                        </li>
                         <li><a href="#"><em className="fas fa-comment-alt nav-icon"></em>Chats<em className="fas fa-circle"></em></a></li>
                         <li><a href="#"><em className="fas fa-bell nav-icon"></em>Notifications<em className="fas fa-circle"></em></a></li>
                         <li><a href="#"><em className="fas fa-pen nav-icon"></em>Create new</a></li>
