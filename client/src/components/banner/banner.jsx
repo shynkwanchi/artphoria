035a8e67b0df49ca0e987a6f3e639f43fb947fc5
Nguyen Quang Duy <duynguyenquang412@gmail.com>
2022-06-25 11:29:56 +0700

    Make the style of the background box reusable

--- client/src/components/banner/banner.jsx
+++ client/src/components/banner/banner.jsx
@@ -3,10 +3,12 @@ import "./banner.css";
 
 const Banner = () => {
     return (
-        <div className="banner-container">
-            <h1>ARTPHORIA</h1>
-            <p>The World of Creativity and Enthusiasm</p>
-            <button className="btn"><a href="#home-main-section">GET STARTED</a></button>
+        <div className="background-container">
+            <div className="banner-content">
+                <h1>ARTPHORIA</h1>
+                <p>The World of Creativity and Enthusiasm</p>
+                <button className="btn"><a href="#home-main-section">GET STARTED</a></button>
+            </div>
         </div>
     );
 }
